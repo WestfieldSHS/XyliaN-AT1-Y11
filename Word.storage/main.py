b08e51a8154fe2db4be56a475729d2df436fb565
xylia(thanh) <xylia.nguyen980@education.nsw.gov.au>
2026-03-16 23:09:49 +0000

    welcome message and json import

--- Word.storage/main.py
+++ Word.storage/main.py
@@ -1,1 +1,11 @@
+import json
 
+with open('vocab.json', 'r', encoding='utf-8') as f:
+    vocab = json.load(f)
+import random
+#Welcome message
+print("Welcome to Vocabulary! Here are some wordd to expand your lexicon:")
+#loop through vocab and print each word and its definition
+for word in random.sample(list(vocab.keys()), min(10, len(vocab))):  # Print only 10 random words/Refresh daily features
+    info = vocab[word]
+    print(f'{word} ({info["type"]}): {info["definition"]}')
