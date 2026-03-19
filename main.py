3b335511efb13f31d31812d6db5bd52061588958
xylia(thanh) <xylia.nguyen980@education.nsw.gov.au>
2026-03-19 23:44:51 +0000

    blank line for better readability

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
-import json
-
-
-
-
-
-
-
-
-
-
 # Vocabulary daily refresh feature
 import json
 
 with open('vocab.json', 'r', encoding='utf-8') as f:
     vocab = json.load(f)
-import random
+#user data handling
+with open('user.json', 'r', encoding='utf-8') as f:
+    user = json.load(f)
+import random #random module for selecting random words for daily refresh feature
 #Welcome message
 print("Welcome to Vocabulary! Here are some wordd to expand your lexicon:")
+print() #blank line for better readability
 #loop through vocab and print each word and its definition
 for word in random.sample(list(vocab.keys()), min(5, len(vocab))):  # Print only 5 random words/Refresh daily features
     info = vocab[word]
     print(f'{word} ({info["type"]}): {info["definition"]}')
+    print() #blank line for better readability
 
