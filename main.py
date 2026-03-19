ca3a37959084bb05556d801c46f762789758437c
xylia(thanh) <xylia.nguyen980@education.nsw.gov.au>
2026-03-19 23:19:34 +0000

    move directory

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ import random
 #Welcome message
 print("Welcome to Vocabulary! Here are some wordd to expand your lexicon:")
 #loop through vocab and print each word and its definition
-for word in random.sample(list(vocab.keys()), min(10, len(vocab))):  # Print only 10 random words/Refresh daily features
+for word in random.sample(list(vocab.keys()), min(5, len(vocab))):  # Print only 10 random words/Refresh daily features
     info = vocab[word]
     print(f'{word} ({info["type"]}): {info["definition"]}')
